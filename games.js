97bb890d0c52a4f086fcbededfd8353115fb91dd
megoti <suikazuzu@gmail.com>
2026-02-17 20:28:17 +0900

    Update game: 餌の気持ち

--- games.js
+++ games.js
@@ -1,13 +1,13 @@
 const gamesData = [
     {
         "id": "game_1770476004719",
-        "title": "é¤ã®æ°æã¡",
+        "title": "餌の気持ち",
         "icon": "games/game_1770476004719/icon.png",
         "url": "https://megoti.github.io/wasatefish/index.html",
-        "description": "é¤ã«ã¯ãªããª",
+        "description": "餌にはなりたくない",
         "isNew": false,
         "views": 0,
-        "date": "2026-02-08",
+        "date": "2026-02-17",
         "genres": [
             "2D",
             "Action"
@@ -15,10 +15,10 @@ const gamesData = [
     },
     {
         "id": "buildingclimber_1770476577158",
-        "title": "Building Climber(ãã«ããç»ãã²ã¼ã )",
+        "title": "Building Climber(Ã£ÂÂÃ£ÂÂ«Ã£ÂÂÃ£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂ²Ã£ÂÂ¼Ã£ÂÂ )",
         "icon": "games/buildingclimber_1770476577158/icon.png",
         "url": "https://ryuki6218.github.io/ClimbingBuilding/building-climber.html",
-        "description": "éå®³ç©ãé¿ããªãããã«ãç»ããï¼ç§»åï¼WASDãããã¯ç¢å°ã­ã¼",
+        "description": "Ã©ÂÂÃ¥Â®Â³Ã§ÂÂ©Ã£ÂÂÃ©ÂÂ¿Ã£ÂÂÃ£ÂÂªÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ«Ã£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂÃ¯Â¼ÂÃ§Â§Â»Ã¥ÂÂÃ¯Â¼ÂWASDÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ¯Ã§ÂÂ¢Ã¥ÂÂ°Ã£ÂÂ­Ã£ÂÂ¼",
         "isNew": true,
         "views": 0,
         "date": "2026-02-07",
@@ -29,10 +29,10 @@ const gamesData = [
     },
     {
         "id": "go_1771325333554",
-        "title": "麺GO! ～ラーメンの妖精引っこ抜きゲーム～",
+        "title": "éººGO! ï½ã©ã¼ã¡ã³ã®å¦ç²¾å¼ã£ãæãã²ã¼ã ï½",
         "icon": "games/go_1771325333554/icon.png",
         "url": "https://men-go-hozu.netlify.app/",
-        "description": "麺をつまんで口に運ぼう！",
+        "description": "éººãã¤ã¾ãã§å£ã«éã¼ãï¼",
         "isNew": false,
         "views": 0,
         "date": "2026-02-17",
