5a967dc1485c9b463ff210b58fd5efa3b291a65c
megoti <s********@gmail.com>
2026-02-17 21:51:00 +0900

    文字化けを修正

--- games.js
+++ games.js
@@ -1,10 +1,10 @@
 const gamesData = [
     {
         "id": "game_1770476004719",
-        "title": "é¤ã®æ°æã¡",
+        "title": "餌の気持ち",
         "icon": "games/game_1770476004719/icon.png",
         "url": "https://megoti.github.io/wasatefish/index.html",
-        "description": "é¤ã«ã¯ãªããããªã",
+        "description": "餌にはなりたくない",
         "isNew": false,
         "views": 0,
         "date": "2026-02-17",
@@ -15,10 +15,10 @@ const gamesData = [
     },
     {
         "id": "buildingclimber_1770476577158",
-        "title": "Building Climber(Ã£ÂÂÃ£ÂÂ«Ã£ÂÂÃ£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂ²Ã£ÂÂ¼Ã£ÂÂ )",
+        "title": "Building Climber(ビルよじ登りゲーム)",
         "icon": "games/buildingclimber_1770476577158/icon.png",
         "url": "https://ryuki6218.github.io/ClimbingBuilding/building-climber.html",
-        "description": "Ã©ÂÂÃ¥Â®Â³Ã§ÂÂ©Ã£ÂÂÃ©ÂÂ¿Ã£ÂÂÃ£ÂÂªÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ«Ã£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂÃ¯Â¼ÂÃ§Â§Â»Ã¥ÂÂÃ¯Â¼ÂWASDÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ¯Ã§ÂÂ¢Ã¥ÂÂ°Ã£ÂÂ­Ã£ÂÂ¼",
+        "description": "障害物を避けながらビルを登ろう！移動：WASDもしくは矢印キー",
         "isNew": false,
         "views": 0,
         "date": "2026-02-17",
