4a52d581f098c96daae7d72c6a7bf20ffb6cec0b
megoti <suikazuzu@gmail.com>
2026-02-17 20:29:33 +0900

    Update game: Building Climber(ビルよじ登りゲーム)

--- games.js
+++ games.js
@@ -1,10 +1,10 @@
 const gamesData = [
     {
         "id": "game_1770476004719",
-        "title": "餌の気持ち",
+        "title": "é¤ã®æ°æã¡",
         "icon": "games/game_1770476004719/icon.png",
         "url": "https://megoti.github.io/wasatefish/index.html",
-        "description": "餌にはなりたくない",
+        "description": "é¤ã«ã¯ãªããããªã",
         "isNew": false,
         "views": 0,
         "date": "2026-02-17",
@@ -15,13 +15,13 @@ const gamesData = [
     },
     {
         "id": "buildingclimber_1770476577158",
-        "title": "Building Climber(Ã£ÂÂÃ£ÂÂ«Ã£ÂÂÃ£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂ²Ã£ÂÂ¼Ã£ÂÂ )",
+        "title": "Building Climber(ビルよじ登りゲーム)",
         "icon": "games/buildingclimber_1770476577158/icon.png",
         "url": "https://ryuki6218.github.io/ClimbingBuilding/building-climber.html",
-        "description": "Ã©ÂÂÃ¥Â®Â³Ã§ÂÂ©Ã£ÂÂÃ©ÂÂ¿Ã£ÂÂÃ£ÂÂªÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ«Ã£ÂÂÃ§ÂÂ»Ã£ÂÂÃ£ÂÂÃ¯Â¼ÂÃ§Â§Â»Ã¥ÂÂÃ¯Â¼ÂWASDÃ£ÂÂÃ£ÂÂÃ£ÂÂÃ£ÂÂ¯Ã§ÂÂ¢Ã¥ÂÂ°Ã£ÂÂ­Ã£ÂÂ¼",
-        "isNew": true,
+        "description": "障害物を避けながらビルを登ろう！移動：WASDもしくは矢印キー",
+        "isNew": false,
         "views": 0,
-        "date": "2026-02-07",
+        "date": "2026-02-17",
         "genres": [
             "2D",
             "Action"
@@ -29,10 +29,10 @@ const gamesData = [
     },
     {
         "id": "go_1771325333554",
-        "title": "éººGO! ï½ã©ã¼ã¡ã³ã®å¦ç²¾å¼ã£ãæãã²ã¼ã ï½",
+        "title": "Ã©ÂºÂºGO! Ã¯Â½ÂÃ£ÂÂ©Ã£ÂÂ¼Ã£ÂÂ¡Ã£ÂÂ³Ã£ÂÂ®Ã¥Â¦ÂÃ§Â²Â¾Ã¥Â¼ÂÃ£ÂÂ£Ã£ÂÂÃ¦ÂÂÃ£ÂÂÃ£ÂÂ²Ã£ÂÂ¼Ã£ÂÂ Ã¯Â½Â",
         "icon": "games/go_1771325333554/icon.png",
         "url": "https://men-go-hozu.netlify.app/",
-        "description": "éººãã¤ã¾ãã§å£ã«éã¼ãï¼",
+        "description": "Ã©ÂºÂºÃ£ÂÂÃ£ÂÂ¤Ã£ÂÂ¾Ã£ÂÂÃ£ÂÂ§Ã¥ÂÂ£Ã£ÂÂ«Ã©ÂÂÃ£ÂÂ¼Ã£ÂÂÃ¯Â¼Â",
         "isNew": false,
         "views": 0,
         "date": "2026-02-17",
